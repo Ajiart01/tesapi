e60f67bbb91590b83ab40a5c690631cda76abc28
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-12 21:33:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs').promises;
-const { igdl, ttdl, fbdown, twitter, youtube } = require('btch-downloader');
-const knights = require('knights-canvas');
-const axios = require('axios')
+import express from 'express';
+import bodyParser from 'body-parser';
+import { promises as fs } from 'fs';
+import { igdl, ttdl, fbdown, twitter, youtube } from 'btch-downloader';
+import knights from 'knights-canvas';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import path from 'path';
+import got from 'got';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = express();
-const cheerio = require('cheerio')
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -14,7 +21,7 @@ router.use(express.static('public')); // Menghubungkan folder statis
 
 // Endpoint untuk halaman dokumentasi
 router.get('/docs', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); // Ganti dengan path file HTML Anda
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ganti dengan path file HTML Anda
 });
 
 router.get('/muslim/quran', async (req, res, next) => {
@@ -40,8 +47,8 @@ router.get('/muslim/tahlil', async (req, res, next) => {
     res.json(loghandler.error);
   }
 });
-// Endpoint untuk mengunduh konten dari Instagram (GET)
 
+// Endpoint untuk mengunduh konten dari Instagram (GET)
 router.get('/api/download/instagram', async (req, res) => {
   const { url } = req.query;
 
@@ -62,33 +69,28 @@ router.get('/api/download/instagram', async (req, res) => {
   }
 });
 
-
-
-
-// Endpoint untuk mengunduh konten dari TikTok (GET
+// Endpoint untuk mengunduh konten dari TikTok (GET)
 router.get('/api/download/tiktok', async (req, res) => {
   const { url } = req.query;
 
   try {
     const data = await ttdl(url); // Anda perlu mengganti downloadTikTok dengan fungsi atau metode yang sesuai untuk mengunduh dari TikTok
-     const response = {
+    const response = {
       result: {
-      title: data.title,
-      title_audio: data.title_audio,
-      thumbnail: data.thumbnail,
-      video: [data.video],
-      audio: [data.audio],
+        title: data.title,
+        title_audio: data.title_audio,
+        thumbnail: data.thumbnail,
+        video: [data.video],
+        audio: [data.audio],
       },
       creator: "BGSBOT RESTAPI"
     };
     res.json(response);
-  
   } catch (error) {
     console.error('Gagal mengunduh dari TikTok:', error);
     res.status(500).json({ error: 'Gagal mengunduh dari TikTok' });
   }
 });
-
 
 // Endpoint untuk mengunduh konten dari Facebook (GET)
 router.get('/api/download/facebook', async (req, res) => {
@@ -96,9 +98,9 @@ router.get('/api/download/facebook', async (req, res) => {
 
   try {
     const data = await fbdown(url);
-      const response = {
+    const response = {
       result: data
-      } 
+    };
     res.json(response);
   } catch (error) {
     console.error('Gagal mengunduh dari Facebook:', error);
@@ -131,32 +133,26 @@ router.get('/api/download/youtube', async (req, res) => {
     res.status(500).json({ error: 'Gagal mengunduh dari YouTube' });
   }
 });
+
 router.get('/api/welcome', async (req, res) => {
   try {
-    // Ambil data dari query params
     const { picurl, name, bgurl, gcname, mem } = req.query;
-
-    // Buat objek Knights Welcome
     const welcomeCard = await new knights.Welcome()
       .setUsername(name || "UNDEFINED")
       .setGuildName(gcname || "WIBU NOLEP")
-      .setGuildIcon("https://i.ibb.co/G5mJZxs/rin.jpg") // Ganti dengan gambar grup Anda jika ada
+      .setGuildIcon("https://i.ibb.co/G5mJZxs/rin.jpg")
       .setMemberCount(mem || "120")
       .setAvatar(picurl || "https://i.ibb.co/1s8T3sY/48f7ce63c7aa.jpg")
       .setBackground(bgurl || "https://i.ibb.co/4YBNyvP/images-76.jpg")
       .toAttachment();
 
-    // Simpan file ke dalam direktori temp
-    const filePath = __dirname + '/tmp/welcome.png'; // Sesuaikan dengan path yang Anda inginkan
+    const filePath = path.join(__dirname, 'tmp', 'welcome.png');
     await fs.writeFile(filePath, welcomeCard.toBuffer());
 
-    // Kirim file sebagai response
     res.sendFile(filePath);
 
-    // Tunggu beberapa saat sebelum menghapus file (opsional)
     await new Promise(resolve => setTimeout(resolve, 3000));
 
-    // Hapus file setelah dikirim
     await fs.unlink(filePath);
   } catch (error) {
     console.error('Error:', error);
@@ -166,34 +162,13 @@ router.get('/api/welcome', async (req, res) => {
 
 router.get('/api/goodbye', async (req, res) => {
   try {
-    // Ambil data dari query params
     const { picurl, name, bgurl, gcname, mem } = req.query;
-    if (!req.query.picurl) return res.status(400).send({ 
-        status: 400, 
-        message: 'picurl tidak boleh kosong!', 
-        result: 'error'
-     })
-     if (!req.query.name) return res.status(400).send({ 
-        status: 400,
-        message: 'name tidak boleh kosong!',
-        result: 'error' 
-    })
-     if (!req.query.bgurl) return res.status(400).send({ 
-        status: 400, 
-        message: 'bgurl tidak boleh kosong!', 
-        result: 'error' 
-    })
-     if (!req.query.gcname) return res.status(400).send({ 
-        status: 400, 
-        message: 'gcname tidak boleh kosong!', 
-        result: 'error' 
-    })
-     if (!req.query.mem) return res.status(400).send({ 
-        status: 400, 
-        message: 'mem tidak boleh kosong!', 
-        result: 'error' 
-    })
-    // Buat objek Knights Goodbye
+    if (!req.query.picurl) return res.status(400).send({ status: 400, message: 'picurl tidak boleh kosong!', result: 'error' });
+    if (!req.query.name) return res.status(400).send({ status: 400, message: 'name tidak boleh kosong!', result: 'error' });
+    if (!req.query.bgurl) return res.status(400).send({ status: 400, message: 'bgurl tidak boleh kosong!', result: 'error' });
+    if (!req.query.gcname) return res.status(400).send({ status: 400, message: 'gcname tidak boleh kosong!', result: 'error' });
+    if (!req.query.mem) return res.status(400).send({ status: 400, message: 'mem tidak boleh kosong!', result: 'error' });
+
     const goodbyeCard = await new knights.Goodbye()
       .setUsername(name || "LING MO")
       .setGuildName(gcname || "Frontline")
@@ -203,17 +178,13 @@ router.get('/api/goodbye', async (req, res) => {
       .setBackground(bgurl || "https://i.ibb.co/G5mJZxs/rin.jpg")
       .toAttachment();
 
-    // Simpan file ke dalam direktori temp
-    const filePath = __dirname + '/tmp/goodbye.png'; // Sesuaikan dengan path yang Anda inginkan
+    const filePath = path.join(__dirname, 'tmp', 'goodbye.png');
     await fs.writeFile(filePath, goodbyeCard.toBuffer());
 
-    // Kirim file sebagai response
     res.sendFile(filePath);
 
-    // Tunggu beberapa saat sebelum menghapus file (opsional)
     await new Promise(resolve => setTimeout(resolve, 3000));
 
-    // Hapus file setelah dikirim
     await fs.unlink(filePath);
   } catch (error) {
     console.error('Error:', error);
@@ -222,194 +193,64 @@ router.get('/api/goodbye', async (req, res) => {
 });
 
 router.get("/maker/jail", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url gambar nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/jail?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
+  const image = req.query.image;
+  if (!image) return res.json("Masukan Url gambar nya");
+  try {
+    const response = await axios.get(`https://api.popcat.xyz/jail?image=${image}`, {
+      responseType: 'arraybuffer'
+    });
+    res.set({ 'Content-Type': 'image/png' });
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/maker/wanted", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url gambar nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/wanted?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
+  const image = req.query.image;
+  if (!image) return res.json("Masukan Url gambar nya");
+  try {
+    const response = await axios.get(`https://api.popcat.xyz/wanted?image=${image}`, {
+      responseType: 'arraybuffer'
+    });
+    res.set({ 'Content-Type': 'image/png' });
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/maker/ship", async (req, res, next) => {
-    const user1 = req.query.user1;
-    if (!user1) return res.json("Masukan Url gambar user1");
-    const user2 = req.query.user2;
-    if (!user2) return res.json("Masukan Url gambar user2");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/ship?user1=${user1}&user2=${user2}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
+  const { image1, image2 } = req.query;
+  if (!image1) return res.json(loghandler.noturl1);
+  if (!image2) return res.json(loghandler.noturl2);
+  try {
+    const response = await axios.get(`https://api.popcat.xyz/ship?user1=${image1}&user2=${image2}`, {
+      responseType: 'arraybuffer'
+    });
+    res.set({ 'Content-Type': 'image/png' });
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/maker/tweetbiden", async (req, res, next) => {
-    const text = req.query.text;
-    if (!text) return res.json("Masukan Text nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/biden?text=${text}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
+router.get('/maker/google-suggestion', async (req, res, next) => {
+  const text = req.query.text;
+  if (!text) return res.json(loghandler.nottext);
+
+  try {
+    const response = await axios.get(`https://api.popcat.xyz/search?q=${text}`, {
+      responseType: 'arraybuffer'
+    });
+    res.set({ 'Content-Type': 'image/png' });
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/maker/drip", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Image nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/drip?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/clown", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Image nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/clown?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/uncover", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Gambar nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/uncover?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/ads", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Gambar nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/ad?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/blur", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Gambar nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/blur?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/invert", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Image nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/invert?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/greyscale", async (req, res, next) => {
-    const image = req.query.image;
-    if (!image) return res.json("Masukan Url Image nya");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/greyscale?image=${image}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/whowin", async (req, res, next) => {
-    const image1 = req.query.image1;
-    if (!image1) return res.json("Masukan Url Gambar 1");
-    const image2 = req.query.image2;
-    if (!image2) return res.json("Masukan Url Gambar 2");
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/whowouldwin?image2=${image2}&image1=${image1}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get("/maker/screenshot", async (req, res, next) => {
-    const url = req.query.url;
-    if (!url) return res.json("Masukan Url");
-
-    try {
-        const response = await axios.get(`https://api.popcat.xyz/screenshot?url=${url}`, {
-            responseType: 'arraybuffer'
-        });
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-
-// Menjalankan server
+// Server listener
 router.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
